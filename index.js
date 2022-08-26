7c4177f97ac9322a56f6eb0722b8d98e181cbb0b
CJSeiler <82341510+CJSeiler@users.noreply.github.com>
2022-08-26 12:26:37 -0400

    added styling / some JS
    tweaked the styling and started working on the functionality of the app

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const tip15Btn = document.getElementById("tip15Btn")
 const tip20Btn = document.getElementById("tip20Btn")
 const tip25Btn = document.getElementById("tip25Btn")
 const customTipBtn = document.getElementById("tipCustomBtn")
+const billInput = document.getElementById("bill")
+
+billInput.addEventListener("keyup", () => {
+   let value = billInput.value
+   if(value.indexOf(".") === -1) return
+    value = toFixedDecimalPlace(value)
+    billInput.value = value
+})
+
+
+// fixes number as type string to 2 decimal places
+function toFixedDecimalPlace(value) { 
+    return parseFloat(value).toFixed(2)
+}
+
