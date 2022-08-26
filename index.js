19ba50fb72b9e90479a27271e85d7482254e7081
CJSeiler <82341510+CJSeiler@users.noreply.github.com>
2022-08-26 14:17:40 -0400

    input functionality
    added input functionality that restricts user input to required lengths

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ const tip20Btn = document.getElementById("tip20Btn")
 const tip25Btn = document.getElementById("tip25Btn")
 const customTipBtn = document.getElementById("tipCustomBtn")
 const billInput = document.getElementById("bill")
+const peopleInput = document.getElementById("people")
 
-billInput.addEventListener("keyup", () => {
-   let value = billInput.value
-   if(value.indexOf(".") === -1) return
-    value = toFixedDecimalPlace(value)
-    billInput.value = value
+billInput.addEventListener("keydown", (e) => {
+    toFixedDecimalPlace(e.target.value, e.target)
+})
+
+customTipBtn.addEventListener("keyup", (e)=> {
+    toFixedLength(e.target.value, e.target)
+})
+
+peopleInput.addEventListener("keyup", (e)=> {
+    if(e.target.value < 0) e.target.value = ""
 })
 
 
 // fixes number as type string to 2 decimal places
-function toFixedDecimalPlace(value) { 
-    return parseFloat(value).toFixed(2)
+function toFixedDecimalPlace(value, target) {
+    if(!value.includes(".")) return value
+
+    let decimal = value.indexOf(".")
+    let number = value.substring(0, decimal)
+    let decimalNumber = value.substring(decimal, value.length)
+    let fixedDecimalNumber = decimalNumber.substring(0, 2)
+    let fixedValue = number + fixedDecimalNumber
+    target.value = fixedValue
+}
+
+function toFixedLength(value, target) {
+    if(!value.length > 3) return
+    
+    target.value = value.slice(0, 3)
 }
 
