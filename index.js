ddc5b078e74976d5c1f37cd99d7662b20c08a1b3
CJSeiler <82341510+CJSeiler@users.noreply.github.com>
2022-08-29 17:33:47 -0400

    functionality
    continued work on the JavaScript functionality of the app

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const tip5Btn = document.getElementById("tip5Btn")
-const tip10Btn = document.getElementById("tip10Btn")
-const tip15Btn = document.getElementById("tip15Btn")
-const tip20Btn = document.getElementById("tip20Btn")
-const tip25Btn = document.getElementById("tip25Btn")
+const tipButtons = document.querySelectorAll("#tipBtn")
 const customTipBtn = document.getElementById("tipCustomBtn")
 const billInput = document.getElementById("bill")
 const peopleInput = document.getElementById("people")
+const tipAmount = document.getElementById("tipAmount")
+const totalAmount = document.getElementById("totalAmount")
+
+tipButtons.forEach(button => {
+    button.addEventListener("click", (e) => selectTipPercentage(e))
+})
+
+function selectTipPercentage(e) {
+    console.log("clicked")
+    tipButtons.forEach(button => {
+        if(button.classList.contains("selected")) {
+            button.classList.remove("selected")
+        } else return
+    })
+    e.target.classList.add("selected")
+}
 
 billInput.addEventListener("keydown", (e) => {
     toFixedDecimalPlace(e.target.value, e.target)
 })
 
-customTipBtn.addEventListener("keyup", (e)=> {
-    toFixedLength(e.target.value, e.target)
-})
+// customTipBtn.addEventListener("keyup", (e)=> {
+//     toFixedLength(e.target.value, e.target)
+// })
 
 peopleInput.addEventListener("keyup", (e)=> {
     if(e.target.value < 0) e.target.value = ""
 })
+
+tipButtons[2].addEventListener("click", calculateTotal)
 
 
 // fixes number as type string to 2 decimal places
@@ -38,3 +52,21 @@ function toFixedLength(value, target) {
     target.value = value.slice(0, 3)
 }
 
+function calculateTipAmount() {
+    const selectedTip = tipButtons.find(button => button.classList.contains("selected")).textContent
+    const tipPercentage = selectedTip / 100
+    const numOfPeople = 4
+    const bill = billInput.value
+    const tipAmount = tipPercentage * bill / numOfPeople
+
+    return Number(tipAmount.toFixed(2))
+}
+
+function calculateTotal() {
+    const bill = billInput.value
+    const numOfPeople = 4
+    const tipAmount = calculateTipAmount()
+    const billTotal = Number(bill / numOfPeople)
+
+    console.log( billTotal + tipAmount)
+}
