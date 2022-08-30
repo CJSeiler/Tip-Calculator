14f613d10fc959d8fcebd337cfc7c93ed6732411
CJSeiler <82341510+CJSeiler@users.noreply.github.com>
2022-08-30 14:26:27 -0400

    continued development of functionality

--- index.js
+++ index.js
@@ -1,72 +1,117 @@
-const tipButtons = document.querySelectorAll("#tipBtn")
-const customTipBtn = document.getElementById("tipCustomBtn")
+const tipButtonsArray = Array.from(document.querySelectorAll(".calculator-tip-btn"))
+const customTipBtn = document.getElementById("customTipBtn")
 const billInput = document.getElementById("bill")
 const peopleInput = document.getElementById("people")
-const tipAmount = document.getElementById("tipAmount")
-const totalAmount = document.getElementById("totalAmount")
+const inputValidity = document.getElementById("inputValidity")
+const tipAmountText = document.getElementById("tipAmount")
+const totalAmountText = document.getElementById("totalAmount")
+const resetBtn = document.getElementById("resetBtn")
 
-tipButtons.forEach(button => {
-    button.addEventListener("click", (e) => selectTipPercentage(e))
+tipButtonsArray.forEach(button => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault()
+        selectTipPercentage(e)
+        calculateTotal()
+    })
 })
 
 function selectTipPercentage(e) {
-    console.log("clicked")
-    tipButtons.forEach(button => {
+    tipButtonsArray.forEach(button => {
         if(button.classList.contains("selected")) {
             button.classList.remove("selected")
         } else return
     })
     e.target.classList.add("selected")
+    
 }
 
-billInput.addEventListener("keydown", (e) => {
-    toFixedDecimalPlace(e.target.value, e.target)
+billInput.addEventListener("keyup", (e) => {
+    e.preventDefault()
+    toFixedDecimalPlace(e.target.value, e.target, 2)
+    calculateTotal()
 })
 
-// customTipBtn.addEventListener("keyup", (e)=> {
-//     toFixedLength(e.target.value, e.target)
-// })
+customTipBtn.addEventListener("keyup", (e)=> {
+    e.preventDefault()
+    toFixedLength(e.target.value, e.target)
+    calculateTotal()
+})
 
 peopleInput.addEventListener("keyup", (e)=> {
-    if(e.target.value < 0) e.target.value = ""
+    e.preventDefault()
+    let {value} = e.target
+    toFixedDecimalPlace(value, e.target, 0)
+    if(value < 1) inputValidity.classList.remove("hide")
+    if(value > 0 || value === "") inputValidity.classList.add("hide") 
+    if(e.key === "-") e.target.value = value.substring(0, value.length)
+    calculateTotal()
 })
 
-tipButtons[2].addEventListener("click", calculateTotal)
+resetBtn.addEventListener("click", resetCalculator)
 
-
-// fixes number as type string to 2 decimal places
-function toFixedDecimalPlace(value, target) {
+// fixes number as type string to given decimal place e.g. 1, 2, 3, etc.
+function toFixedDecimalPlace(value, target, decimalPlace) {
     if(!value.includes(".")) return value
 
-    let decimal = value.indexOf(".")
-    let number = value.substring(0, decimal)
-    let decimalNumber = value.substring(decimal, value.length)
-    let fixedDecimalNumber = decimalNumber.substring(0, 2)
+    let decimalIndex = value.indexOf(".")
+    let number = value.substring(0, decimalIndex)
+    let decimalNumber = value.substring(decimalIndex, value.length)
+    let fixedDecimalNumber = decimalNumber.substring(0, decimalPlace + 1)
     let fixedValue = number + fixedDecimalNumber
     target.value = fixedValue
 }
 
 function toFixedLength(value, target) {
-    if(!value.length > 3) return
-    
-    target.value = value.slice(0, 3)
+    if(!value.length > 2) return
+    target.value = value.substring(0,2)
+}
+
+function calculateTipPercentage() {
+    const selectedTip = tipButtonsArray.find(button => button.classList.contains("selected"))
+    let selectedTipNumber
+
+    if(selectedTip === customTipBtn) {
+        return selectedTipNumber = Number(selectedTip.value)
+    } else {
+        const percentIndex = selectedTip.textContent.indexOf("%")
+        return selectedTipNumber = Number(selectedTip.textContent.substring(0, percentIndex))
+    }
+
 }
 
 function calculateTipAmount() {
-    const selectedTip = tipButtons.find(button => button.classList.contains("selected")).textContent
-    const tipPercentage = selectedTip / 100
-    const numOfPeople = 4
-    const bill = billInput.value
+    const tipPercentage = calculateTipPercentage() / 100
+    const numOfPeople = Number(peopleInput.value)
+    const bill = Number(billInput.value)
     const tipAmount = tipPercentage * bill / numOfPeople
 
+    tipAmountText.textContent = `$${tipAmount.toFixed(2)}`
     return Number(tipAmount.toFixed(2))
 }
 
 function calculateTotal() {
-    const bill = billInput.value
-    const numOfPeople = 4
+    const bill = Number(billInput.value)
+    const numOfPeople = Number(peopleInput.value)
+    if(numOfPeople < 1) {
+        tipAmountText.textContent = "$0.00"
+        totalAmountText.textContent = "$0.00"
+        return
+    }
     const tipAmount = calculateTipAmount()
-    const billTotal = Number(bill / numOfPeople)
+    const billTotal = (bill + tipAmount) / numOfPeople
 
-    console.log( billTotal + tipAmount)
+    totalAmountText.textContent = `$${billTotal.toFixed(2)}`
+}
+
+function resetCalculator() {
+    tipAmountText.textContent = "$0.00"
+    totalAmountText.textContent = "$0.00"
+    billInput.value=""
+    peopleInput.value="0"
+
+    tipButtonsArray.forEach(button => {
+        button.classList.remove("selected")
+    })
+
+    billInput.focus()
 }
